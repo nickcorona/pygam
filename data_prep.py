57238c1fd9956573a12571a979f406f37f979f75
Nick Corona <nicklucianocorona@gmail.com>
2019-04-20 00:08:39 -0400

    Dropped `o3median`.

--- data_prep.py
+++ data_prep.py
@@ -4,9 +4,12 @@ import pickle
 df = pd.read_pickle('data/raw/chicago.pickle')
 print('Read chicago dataset.')
 
-df = df.drop('pm25median', axis=1).dropna(how='any')
+# dropping `pm25median' because of too many missings
+# dropping `o3median` because it is insignificant to the GAM model
+drop_features = ['pm25median', 'o3median']
+df = df.drop(drop_features, axis=1).dropna(how='any')
 
-print('Dropped `pm25median` and then any rows with missing values.')
+print(f'Dropped {drop_features} and then any rows with missing values.')
 
 y = df['death'].to_numpy()
 X = df.iloc[:, 1:].to_numpy()
